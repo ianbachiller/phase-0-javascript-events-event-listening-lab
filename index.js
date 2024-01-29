c7802424f7e20a495a65c3fb0d613c64920a749c
Ian Bachiller <ianbachise@gmail.com>
2024-01-28 23:56:49 -0500

    completes js event listening lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click',function(){
+        // console.log("I was clicked yah bitches!")
+        alert("'Click me button' was clicked! Achievement! Allez!");
+    });
+
+
 }
+addingEventListener();
